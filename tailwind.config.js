a6aa380f85c0e10431a59d0b0f4fb336287a5fae
samata saha <samatasaha03@gmail.com>
2024-10-26 14:05:38 +0530

    26/10/24 hero section done 1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        sm:"0.75rem",
+        base:"0.875rem",
+        md:"1rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem",
+        "4xl":"3rem",
+        "5xl":"4rem"
+      },
+    },
   },
   plugins: [],
 }
